f2cda89a8b1d16390fc335f7311b5d905aef4e17
Ryan Davis <ryand-ruby@zenspider.com>
2019-05-23 14:35:25 -0700

    Fix CLI version command test to be resilient on a bad network.
    Also switched out LatestInSpecVersion.latest to be much cleaner using open-uri.
    
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- lib/utils/latest_version.rb
+++ lib/utils/latest_version.rb
@@ -1,19 +1,13 @@
 # author: Christoph Hartmann
 
 require 'json'
-require 'net/http'
+require 'open-uri'
 
 class LatestInSpecVersion
   # fetches the latest version from rubygems server
   def latest
     uri = URI('https://rubygems.org/api/v1/gems/inspec.json')
-    res = Net::HTTP.start(
-      uri.host, uri.port, use_ssl: uri.scheme == 'https',
-      open_timeout: 0.5, read_timeout: 0.5
-    ) { |http|
-      http.get(uri.path)
-    }
-    inspec_info = JSON.parse(res.body)
+    inspec_info = JSON.parse(uri.read)
     inspec_info['version']
   rescue StandardError
     nil
